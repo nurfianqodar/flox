410415b7ca1b5a894c20222393445ad21fb63273
nurfianqodar <77nurfianqodar@gmail.com>
2026-08-01 20:30:52 +0700

    change version on build.zig.zon and tidy some code

--- src/main.zig
+++ src/main.zig
@@ -198,8 +198,7 @@ const Command = union(Tag) {
             defer ifile.close(io);
 
             const opath = options.output orelse ipath;
-            if (try flox.isFileExists(io, opath) and !options.force)
-                return error.PathAlreadyExists;
+            if (try flox.isFileExists(io, opath) and !options.force) return error.PathAlreadyExists;
             var ofile = try cwd.createFileAtomic(io, opath, .{ .replace = true });
             defer ofile.deinit(io);
 
