2d301faa5a45f5c7828d38e63fde6f4d6718f269
nurfianqodar <77nurfianqodar@gmail.com>
2026-08-05 13:51:16 +0700

    add encrypt decrypt test

--- src/flox.zig
+++ src/flox.zig
@@ -11,3 +11,51 @@ pub const decryptStream = decryptor.stream;
 const utils = @import("flox/utils.zig");
 pub const isFileExists = utils.isFileExists;
 pub const isFloxFile = utils.isFloxFile;
+
+test "encrypt and decrypt stream" {
+    const std = @import("std");
+    const t = std.testing;
+    const io = t.io;
+    const allocator = t.allocator;
+
+    const plain_path = "tests/test-data.svg";
+    const enc_path = "tests/test-data.svg.flox";
+    const dec_path = "tests/test-data-dec.svg";
+    const password = "secretpassword";
+    var cwd = std.Io.Dir.cwd();
+
+    {
+        const meta: Cipher.Metadata = try .init(io, .{ .m = 1024, .t = 1, .p = 1 });
+        var cipher: Cipher = try .init(io, allocator, meta, password);
+        defer cipher.deinit();
+
+        var plain_file = try cwd.openFile(io, plain_path, .{ .mode = .read_only });
+        defer plain_file.close(io);
+
+        var enc_file = try cwd.createFile(io, enc_path, .{ .truncate = true });
+        defer enc_file.close(io);
+
+        try encryptStream(io, allocator, &plain_file, &enc_file, &cipher, 1024);
+        try enc_file.sync(io);
+    }
+
+    {
+        var enc_file = try cwd.openFile(io, enc_path, .{ .mode = .read_only });
+        defer enc_file.close(io);
+
+        var dec_file = try cwd.createFile(io, dec_path, .{ .truncate = true });
+        defer dec_file.close(io);
+
+        try decryptStream(io, allocator, &enc_file, &dec_file, password);
+
+        try dec_file.sync(io);
+    }
+
+    const plain = try cwd.readFileAlloc(io, plain_path, allocator, .unlimited);
+    defer allocator.free(plain);
+
+    const dec = try cwd.readFileAlloc(io, dec_path, allocator, .unlimited);
+    defer allocator.free(dec);
+
+    try t.expectEqualSlices(u8, plain, dec);
+}
